e5c0f08e077fec941b5582f7aa9d2e986bb53523
Jordan Agosta <jordanagosta@csu.fullerton.edu>
2026-03-05 22:24:42 -0800

    Add Day 4 Part I & II file

---
+++ Day 4_ptI&II.R
@@ -0,0 +1,85 @@
+hints <- read.csv("HINTSData_2020_clean.csv")
+#Loads HINTS data set into a variable that we can use
+
+barplot(table(hints$QualityCare),
+        main = "Distribution of QualityCare",
+        xlab = "QualityCare",
+        ylab = "Frequency",
+        col = "lightblue")
+#display bar graph one showing distribution of QualityCare
+#graph displays distribution of respondents with their
+#quality care rating
+barplot(table(hints$RaceEthn5),
+        main = "Distribution of RaceEthn5",
+        xlab = "RaceEthn5",
+        ylab = "Frequency",
+        col = "lightgreen")
+#display graph one showing distribution of RaceEthn5
+#graph displays distribution of respondents with their
+#RaceEthn5 rating
+
+hist(table(hints$Age),
+     main = "Distribution of Age",
+     xlab = "Age",
+     ylab = "Frequency",
+     col = "yellow")
+#display bar graph one showing distribution of Age
+#graph displays distribution of respondents with their age
+hist(table(hints$BMI),
+     main = "Distribution of BMI",
+     xlab = "BMI",
+     ylab = "Frequency",
+     col = "orange")
+#display graph one showing distribution of BMI
+#graph displays distribution of respondents with their
+#BMI rating
+
+library(ggplot2)
+#load the ggplot2 package for advanced data visualization 
+ggplot(hints_subset, aes(x=Age)) + 
+  geom_histogram()
+#histogram of age 
+#this graph displays the distribution of respondents' ages 
+#a histogram is appropriate because Age is a continuos variable 
+
+ggplot(hints_subset, aes(x=BMI)) + 
+  geom_histogram()
+#histogram of BMI 
+#shoes how BMI values are distributed among participants 
+#it helps identify patterns such as normal distribution or skewness 
+
+ggplot(hints_subset, aes(x=QualityCare)) + 
+  geom_bar()
+#bar chart for QualityCare 
+#graph shoes the frequency of each perceived healthcare quality rating 
+
+ggplot(hints_subset, aes(x=RaceEthn5)) + 
+  geom_bar()
+#bar chart for Race/Ethnicity, observes groups that are most represented in data
+
+
+my_visual_func_I <- function(data, variable){
+  barplot(table(data[[variable]]),
+          main = paste("Distribution of", variable),
+          xlab = variable,
+          ylab = "Frequency")
+}
+#bar chart for categorical variables 
+#calculates category frequencies 
+
+my_visual_func_I(hints_subset, "QualityCare")
+#bar chart for QualityCare
+
+my_visual_func_I(hints_subset, "RaceEthn5")
+#bar chart for Race/Ethnicity 
+
+my_visual_funn_II <-function(data, variable){
+  hist(data[[variable]],
+       main=paste("Distribution of", variable),
+       xlab = variable)
+}
+#histogram for continuous variables and distribution of numeric data 
+
+my_visual_funn_II(hints_subset, "Age")
+my_visual_funn_II(hints_subset, "BMI")
+#comman generates histogram for continous variables 
